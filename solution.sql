0fe5e9019ca6c32d06f5cfca3435dfa13615f77f
JQUAN COLLINS <104362301+jquan114@users.noreply.github.com>
2023-07-06 20:01:26 -0400

    Create solution.sql

---
+++ solution.sql
@@ -0,0 +1,21 @@
+-- Table: Weather
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | id            | int     |
+-- | recordDate    | date    |
+-- | temperature   | int     |
+-- +---------------+---------+
+-- id is the primary key for this table.
+-- This table contains information about the temperature on a certain day.
+ 
+
+-- Write an SQL query to find all dates' Id with higher temperatures compared to its previous dates (yesterday).
+
+-- Return the result table in any order.
+
+SELECT w1.id
+FROM Weather w1
+JOIN Weather w2 ON w1.recordDate = DATE_ADD(w2.recordDate, INTERVAL 1 DAY)
+WHERE w1.temperature > w2.temperature
